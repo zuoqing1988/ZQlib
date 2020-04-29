9ef7380274e31560337a7e0ebef5fe9286d03297
左庆 <zuoqing1988@qq.com>
2020-04-29 12:51:54 +0800

    优化驾照矫正

--- SampleRectifyCard/SampleRectifyCard.cpp
+++ SampleRectifyCard/SampleRectifyCard.cpp
@@ -20,15 +20,11 @@ void test_rect()
 	}
 }
 
+
 void test_drivercard()
 {
 	cv::Mat img = cv::imread("7.jpg");
 	const char* out_name = "dst.jpg";
-	cv::Rect red_roi(cv::Point(52, 146), cv::Point(151, 243));
-	cv::Rect c1_roi(cv::Point(184, 200), cv::Point(310, 240));
-	/*cv::Mat img = cv::imread("1.jpg");
-	cv::Rect red_roi(cv::Point(42, 241), cv::Point(194, 394));
-	cv::Rect c1_roi(cv::Point(260, 340), cv::Point(461, 397));*/
 	cv::Mat output;
 
 	int dstWidth = 440;
@@ -36,17 +32,17 @@ void test_drivercard()
 
 	int rects[11][4] =
 	{
-		{175,49,371,71},
-		{52,76,194,101},
-		{238,76,277,101},
-		{318,76,417,101},
-		{52,105,414,132},
-		{52,126,414,159},
-		{178,158,311,188},
-		{201,192,311,217},
-		{183,225,311,252},
-		{87,254,192,283},
-		{213,256,319,283}
+		{ 175,50,371,75 },		//证号
+		{ 54,78,194,105 },		//姓名
+		{ 238,78,277,105 },		//性别
+		{ 318,78,417,105 },		//国籍
+		{ 52,105,414,132 },		//地址第一行
+		{ 52,132,414,159 },		//地址第二行
+		{ 178,160,311,190 },	//生日
+		{ 203,194,311,220 },	//发证日期
+		{ 183,225,311,254 },	//类型
+		{ 87,256,192,285 },		//有效期第一格
+		{ 213,256,325,285 }		//有效期第二格
 	};
 	dstWidth *= 1.0;
 	dstHeight *= 1.0;
@@ -62,9 +58,13 @@ void test_drivercard()
 	bool display = false;
 #endif
 
-	
-	if (ZQ_RectifyCard::RectifyDriverCard(img, dstWidth, dstHeight, output, red_roi, c1_roi, display))
+	cv::Mat transmat;
+	cv::Mat smooth_img;
+	img.copyTo(smooth_img);
+	cv::GaussianBlur(smooth_img, smooth_img, cv::Size(3, 3), 0);
+	if (ZQ_RectifyCard::RectifyDriverCard(smooth_img, dstWidth, dstHeight, output, transmat, 50, display))
 	{
+		cv::warpPerspective(img, output, transmat, cv::Size(dstWidth, dstHeight));
 		for (int i = 0; i < 11; i++)
 		{
 			cv::rectangle(output, cv::Rect(cv::Point(rects[i][0], rects[i][1]), cv::Point(rects[i][2], rects[i][3])), cv::Scalar(0, 0, 255), 1);
@@ -92,8 +92,8 @@ void test_IDcard()
 #else
 	bool display = false;
 #endif
-
-	if (ZQ_RectifyCard::RectifyCard(img, dstWidth, dstHeight, output, 50, display))
+	cv::Mat transmat;
+	if (ZQ_RectifyCard::RectifyCard(img, dstWidth, dstHeight, output, transmat, 50, display))
 	{
 		cv::imwrite("3_dst.jpg", output);
 		cv::namedWindow("src");
@@ -108,8 +108,8 @@ void test_IDcard()
 int main()
 {
 	//test_rect();
-	//test_drivercard();
-	test_IDcard();
+	test_drivercard();
+	//test_IDcard();
 	
 	return 0;
 }
