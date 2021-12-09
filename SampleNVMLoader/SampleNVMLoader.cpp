7934987d2370fdc4312032155abe139fa7d67b8d
zuoqing1988 <zuoqing1988@qq.com>
2021-12-09 19:03:29 +0800

    增加从NVM文件提取指定图片的点云信息

--- SampleNVMLoader/SampleNVMLoader.cpp
+++ SampleNVMLoader/SampleNVMLoader.cpp
@@ -5,34 +5,50 @@
 #include "ZQ_NVMLoader.h"
 using namespace ZQ;
 
+int extract_camera_info(const char* nvm_file, const char* out_file);
+
+int extract_points_info_of_camera(const char* nvm_file, const char* came_name, const char* out_file);
+
+
 int main(int argc, const char** argv)
 {
-	if (argc != 3)
+	if (argc != 3 && argc != 4)
 	{
 		printf(".exe nvm_file out_file\n");
-		return -1;
+		printf(".exe nvm_file came_name out_file\n");
+		return EXIT_FAILURE;
 	}
-	const char* nvm_file = argv[1];
-	const char* out_file = argv[2];
+	if (argc == 3)
+	{
+		return extract_camera_info(argv[1], argv[2]);
+	}
+	else if (argc == 4)
+	{
+		return extract_points_info_of_camera(argv[1], argv[2], argv[3]);
+	}
+	return EXIT_FAILURE;
+}
 
+int extract_camera_info(const char* nvm_file, const char* out_file)
+{
 	ZQ_NVM_Container container;
 	if (!container.LoadFromFile(nvm_file))
 	{
 		printf("failed to load nvm file %s\n", nvm_file);
-		return -1;
+		return EXIT_FAILURE;
 	}
 
 	if (container.model_list.size() == 0)
 	{
 		printf("no model is in nvm file %s\n", nvm_file);
-		return -1;
+		return EXIT_FAILURE;
 	}
 
 	FILE* out = fopen(out_file, "w");
 	if (out == 0)
 	{
 		printf("failed to create file %s\n", out_file);
-		return -1;
+		return EXIT_FAILURE;
 	}
 
 	int ncam = container.model_list[0].camera_list.size();
@@ -46,5 +62,21 @@ int main(int argc, const char** argv)
 
 	fclose(out);
 	printf("succ to save %s\n", out_file);
-	return 0;
+	return EXIT_SUCCESS;
+}
+
+int extract_points_info_of_camera(const char* nvm_file, const char* came_name, const char* out_file)
+{
+	ZQ_NVM_Container container;
+	if (!container.LoadFromFile(nvm_file))
+	{
+		printf("failed to load nvm file %s\n", nvm_file);
+		return EXIT_FAILURE;
+	}
+
+	if (!container.ExportPointsOfOneImage(came_name, out_file))
+	{
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
 }
